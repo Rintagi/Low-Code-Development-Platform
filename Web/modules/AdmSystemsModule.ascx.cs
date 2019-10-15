b8f4d940b8407a85e794d7b6d04cd14382bbb8fa
gary <gary.ng@robocoder.com>
2019-10-15 12:42:42 -0700

    revised React template files(shared components etc.) as well as generated code(for individual pages) to remove eslint warnings, better rintagi.js definition for localhost:3000 dev(3 tier)

--- Web/modules/AdmSystemsModule.ascx.cs
+++ Web/modules/AdmSystemsModule.ascx.cs
@@ -1746,8 +1746,14 @@ string.Format(@"
  * for reactjs configuration, make sure homepage is set to './' so everything generated is relative 
  */
 document.Rintagi = {{
+  localDev:{{
+    // these setup is only effective when served via app is served via http://localhost:3000 type, for local npm start development. ignored in production build or proxying to localhost
+    appNS:{4},
+    appDomainUrl:'http://{2}/{0}', // master domain this app is targetting, empty/null means the same as apiBasename, no ending slash, design for multiple api endpoint usage(js hosting not the same as webservice hosting)
+    apiBasename: 'http://{2}/{0}', // webservice url for local development via npm start, i.e. localhost:3000 etc. must be full url in the form of http:// pointing to the site serving , no ending slash
+  }},
   appRelBase:['React','ReactProxy','ReactPort'],  // path this app is serving UNDER(can be multiple), implicitly assume they are actually /Name/, do not put begin/end slash 
-  appNS:'',
+  appNS:'', // used for login token sync(shared login when served under the same domain) between apps and asp.net site
   appDomainUrl:'', // master domain this app is targetting, empty/null means the same as apiBasename, no ending slash, design for multiple api endpoint usage(js hosting not the same as webservice hosting)
   apiBasename: '', // webservice url, can be relative or full http:// etc., no ending slash
   useBrowserRouter: false,    // whether to use # based router(default) or standard browser based router(set to true, need server rewrite support, cannot be used for CDN or static file directory)
