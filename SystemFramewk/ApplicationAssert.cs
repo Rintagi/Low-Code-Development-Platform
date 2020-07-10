ec9fb6495dc6f4d92417aaf03f184dac427972f4
robocoder <cs@robocoder.com>
2020-07-10 06:59:04 -0700

    Squashed commit of the following:
    commit e1f441146d56c9665aa90af46bfa1b3291af8ece
    Author: gary <gary.ng@robocoder.com>
    Date:   Fri Jul 10 06:47:01 2020 -0700
    
        Squashed commit of the following:
    
        commit ae8995f4a223bc3ee52fc0ad5e26053d414b2568
        Author: gary <gary.ng@robocoder.com>
        Date:   Thu Jul 9 16:26:04 2020 -0700
    
            remove ODBC driver noise from error message
    
    commit 7dedcd1f57485f64d598036e06901c4aef88eaba
    Author: gary <gary.ng@robocoder.com>
    Date:   Fri Jul 10 06:43:58 2020 -0700
    
        R11.197.00710 community installer

--- SystemFramewk/ApplicationAssert.cs
+++ SystemFramewk/ApplicationAssert.cs
@@ -2,13 +2,18 @@ namespace RO.SystemFramewk
 {
 	using System;
 	using System.Diagnostics;
+    using System.Text.RegularExpressions;
 
 	public class ApplicationAssert
 	{
+        private static Regex formatter = new Regex(@"ERROR\s*\[\d+\].*\[Microsoft\]\[ODBC.+\]\[SQL Server\]");
 		public static void CheckCondition(bool condition, string sourceText, string causeText, string errorText)
 		{
 			if (!condition)
 			{
+                // remove driver related info from mesage
+                errorText = formatter.Replace(errorText, "");
+
 				if (causeText != string.Empty) {errorText = causeText + ": " + errorText;}
 				if (sourceText != string.Empty) {errorText = sourceText + ": " + errorText;}
 				throw new ApplicationException(errorText);
